46a71be8a2c8be6f88a59e2dc3edafb60c863eac
gursewakinsan <insagursewakinsa@gmail.com>
2022-09-05 21:32:40 +0530

    Add font family for app

--- WareHousePickPack/AssemblyInfo.cs
+++ WareHousePickPack/AssemblyInfo.cs
@@ -1,3 +1,17 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("PoppinsBold.ttf", Alias = "PoppinsBoldFontFamily")]
+[assembly: ExportFont("PoppinsRegular.ttf", Alias = "PoppinsRegularFontFamily")]
+[assembly: ExportFont("PoppinsSemiBold.ttf", Alias = "PoppinsSemiBoldFontFamily")]
+[assembly: ExportFont("PoppinsMedium.ttf", Alias = "PoppinsMediumFontFamily")]
+
+[assembly: ExportFont("WorkSansBold.ttf", Alias = "WorkSansBoldFontFamily")]
+[assembly: ExportFont("WorkSansExtraBold.ttf", Alias = "WorkSansExtraBoldFontFamily")]
+[assembly: ExportFont("WorkSansMedium.ttf", Alias = "WorkSansMediumFontFamily")]
+[assembly: ExportFont("WorkSansRegular.ttf", Alias = "WorkSansRegularFontFamily")]
+[assembly: ExportFont("WorkSansSemiBold.ttf", Alias = "WorkSansSemiBoldFontFamily")]
+
+[assembly: ExportFont("MaterialDesignIcons.ttf", Alias = "MaterialIconsFontFamily")]
